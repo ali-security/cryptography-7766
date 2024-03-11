62b6f1506190f260c025bdadddfc91e10e9fcc3a
Amit Agam <amit.a@sealsecurity.io>
2024-03-11 11:45:56 +0200

    ignore fallible conversions

--- src/rust/src/pool.rs
+++ src/rust/src/pool.rs
@@ -35,7 +35,7 @@ impl FixedPool {
         })
     }
 
-    #[allow(clippy::unnecessary_fallible_conversions)] fn acquire(slf: pyo3::Py<Self>, py: pyo3::Python<'_>) -> pyo3::PyResult<PoolAcquisition> {
+    fn acquire(slf: pyo3::Py<Self>, py: pyo3::Python<'_>) -> pyo3::PyResult<PoolAcquisition> {
         let v = slf.as_ref(py).borrow().value.replace(None);
         if let Some(value) = v {
             Ok(PoolAcquisition {
